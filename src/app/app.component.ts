fbb28276f5b5cbe55c497c07d68dcd041684bbe1
466548 <tsuka.as@gmail.com>
2018-12-16 14:56:13 +0900

    add 書籍情報

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,23 @@ import { Book } from './book';
 })
 export class AppComponent {
   selected : Book;
-  title = 'http-test';
+
+  books = [
+    {
+      isbn: '978-4-7741-8411-1',
+      title: '改訂新版JavaScript本格入門',
+      price: 2980,
+      publisher: '技術評論社',
+    },
+    {
+      isbn: '978-4-7980-4853-6',
+      title: 'はじめてのAndroidアプリ開発 第2版',
+      price: 3200,
+      publisher: '秀和システム',
+    }
+  ];
+  //リンクされた時に選択された書籍情報をselectedプロパティに保存
+  onclick(book: Book) {
+    this.selected = book;
+  }
 }
